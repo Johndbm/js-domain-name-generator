f2b9e9b56c5fd7f18f243da653f184daddf2ae0d
john.banks <johndbanksm@gmail.com>
2022-11-15 16:12:57 +0000

    Fixed: switch code between branches (main ==> dev & viceversa)

--- app.js
+++ app.js
@@ -1,16 +1,14 @@
 let pronoun = ['the','our'];
 let adj = ['great', 'big' ];
 let noun = ['jogger','racoon'];
-let toplvldomain = ['.com','.us','.tk','.org','.biz','.eth','.net','.ve']
+
 
 for (let i = 0; i < pronoun.length; i++) {
     for (let i = 0; i < adj.length; i++) {
         for (let i = 0; i < noun.length; i++) {
-            for (let i = 0; i < toplvldomain.length; i++) {
-                console.log (`${pronoun[i]}${adj[i]}${noun[i]}${toplvldomain[i]}`)
-            }
+            console.log (`${pronoun[i]}${adj[i]}${noun[i]}.com`)
         }
     }
     
 }
-//console.log("any string")
+//console.log("any string")main
