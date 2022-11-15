9ad1c3e5b966f7fd8210bec7c49cbf926f63c562
john.banks <johndbanksm@gmail.com>
2022-11-15 16:25:43 +0000

    :bug::construction: Fixed variables ambiguity

--- app.js
+++ app.js
@@ -4,9 +4,9 @@ let noun = ['jogger','racoon'];
 
 
 for (let i = 0; i < pronoun.length; i++) {
-    for (let i = 0; i < adj.length; i++) {
-        for (let i = 0; i < noun.length; i++) {
-            console.log (`${pronoun[i]}${adj[i]}${noun[i]}.com`)
+    for (let j = 0; j < adj.length; j++) {
+        for (let k = 0; k < noun.length; k++) {
+            console.log (`${pronoun[i]}${adj[j]}${noun[k]}.com`)
         }
     }
     
