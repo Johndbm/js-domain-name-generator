4ef6081517e8b8d183fc8d4702385c3411c29337
john.banks <johndbanksm@gmail.com>
2022-11-15 00:14:49 +0000

    playing around with the for loops

---
+++ app.js
@@ -0,0 +1,9 @@
+let pronoun = ['the','our'];
+let adj = ['great', 'big' ];
+let noun = ['jogger','racoon'];
+
+for (let i = 0; i < pronoun.length; i++) {
+    let domainRandom = pronoun[i];
+    
+}
+console.log(domainRandom[i])
